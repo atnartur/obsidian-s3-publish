b54cc7027b5c93fa6abfe52e8ce68ea68fbff023
atnartur <i@atnartur.dev>
2025-01-03 19:49:43 +0000

    + s3 uploads working

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,8 +1,12 @@
-import {src, dest} from 'gulp';
+import {src, dest, watch as gulpWatch} from 'gulp';
 import path from "node:path";
 import manifest from './manifest.json' with { type: "json" };
 
 export function copy() {
 	return src(['main.js', 'manifest.json'])
 		.pipe(dest(path.join(process.env.VAULT_DIR, '.obsidian', 'plugins', manifest.id)));
+}
+
+export function watch() {
+    gulpWatch('main.js', copy);
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-import {App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	requestUrl,
+	RequestUrlParam,
+	Setting
+} from 'obsidian';
 import {Upload} from "@aws-sdk/lib-storage";
 import {S3Client} from "@aws-sdk/client-s3";
 import {generateHtml} from "./html-generator";
+import {FetchHttpHandler, FetchHttpHandlerOptions} from "@smithy/fetch-http-handler";
+import {HttpHandlerOptions} from "@smithy/types";
+import { type HttpRequest, HttpResponse } from "@smithy/protocol-http";
+import { buildQueryString } from "@smithy/querystring-builder";
+import { requestTimeout } from "@smithy/fetch-http-handler/dist-es/request-timeout";
 
 interface PDFPublisherSettings {
 	awsAccessKeyId: string;
@@ -15,6 +29,156 @@ const DEFAULT_SETTINGS: PDFPublisherSettings = {
 	awsSecretAccessKey: '',
 	bucketName: '',
 	region: 'us-east-1'
+}
+
+/**
+ * https://stackoverflow.com/questions/8609289
+ * @param b Buffer
+ * @returns ArrayBuffer
+ */
+export const bufferToArrayBuffer = (
+  b: Buffer | Uint8Array | ArrayBufferView
+) => {
+  return b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength);
+};
+
+class ObsHttpHandler extends FetchHttpHandler {
+  requestTimeoutInMs: number | undefined;
+  reverseProxyNoSignUrl: string | undefined;
+  constructor(
+    options?: FetchHttpHandlerOptions,
+    reverseProxyNoSignUrl?: string
+  ) {
+    super(options);
+    this.requestTimeoutInMs =
+      options === undefined ? undefined : options.requestTimeout;
+    this.reverseProxyNoSignUrl = reverseProxyNoSignUrl;
+  }
+  async handle(
+    request: HttpRequest,
+    { abortSignal }: HttpHandlerOptions = {}
+  ): Promise<{ response: HttpResponse }> {
+    if (abortSignal?.aborted) {
+      const abortError = new Error("Request aborted");
+      abortError.name = "AbortError";
+      return Promise.reject(abortError);
+    }
+
+    let path = request.path;
+    if (request.query) {
+      const queryString = buildQueryString(request.query);
+      if (queryString) {
+        path += `?${queryString}`;
+      }
+    }
+
+    const { port, method } = request;
+    let url = `${request.protocol}//${request.hostname}${
+      port ? `:${port}` : ""
+    }${path}`;
+    if (
+      this.reverseProxyNoSignUrl !== undefined &&
+      this.reverseProxyNoSignUrl !== ""
+    ) {
+      const urlObj = new URL(url);
+      urlObj.host = this.reverseProxyNoSignUrl;
+      url = urlObj.href;
+    }
+    const body =
+      method === "GET" || method === "HEAD" ? undefined : request.body;
+
+    const transformedHeaders: Record<string, string> = {};
+    for (const key of Object.keys(request.headers)) {
+      const keyLower = key.toLowerCase();
+      if (keyLower === "host" || keyLower === "content-length") {
+        continue;
+      }
+      transformedHeaders[keyLower] = request.headers[key];
+    }
+
+    let contentType: string | undefined = undefined;
+    if (transformedHeaders["content-type"] !== undefined) {
+      contentType = transformedHeaders["content-type"];
+    }
+
+    let transformedBody: any = body;
+    if (ArrayBuffer.isView(body)) {
+      transformedBody = bufferToArrayBuffer(body);
+    }
+
+    const param: RequestUrlParam = {
+      body: transformedBody,
+      headers: transformedHeaders,
+      method: method,
+      url: url,
+      contentType: contentType,
+    };
+
+    const raceOfPromises = [
+      requestUrl(param).then((rsp) => {
+        const headers = rsp.headers;
+        const headersLower: Record<string, string> = {};
+        for (const key of Object.keys(headers)) {
+          headersLower[key.toLowerCase()] = headers[key];
+        }
+        const stream = new ReadableStream<Uint8Array>({
+          start(controller) {
+            controller.enqueue(new Uint8Array(rsp.arrayBuffer));
+            controller.close();
+          },
+        });
+        return {
+          response: new HttpResponse({
+            headers: headersLower,
+            statusCode: rsp.status,
+            body: stream,
+          }),
+        };
+      }),
+      requestTimeout(this.requestTimeoutInMs),
+    ];
+
+    if (abortSignal) {
+      raceOfPromises.push(
+        new Promise<never>((resolve, reject) => {
+          abortSignal.onabort = () => {
+            const abortError = new Error("Request aborted");
+            abortError.name = "AbortError";
+            reject(abortError);
+          };
+        })
+      );
+    }
+    return Promise.race(raceOfPromises);
+  }
+}
+
+class S3RequestHandler extends FetchHttpHandler {
+	async handle(httpRequest: HttpRequest, options?: HttpHandlerOptions): Promise<{ response: HttpResponse }> {
+		console.log('original request', httpRequest);
+		console.log('original options', options);
+
+		const plainQueryObject: {[key: string]: any} = {};
+		Object.entries(httpRequest.query)
+			// skip some headers as it was in
+			// https://github.com/remotely-save/remotely-save/blob/master/src/fsS3.ts#L94C24-L94C41
+			.filter(([key]) => !['host', 'content-length'].contains(key.toLowerCase()))
+			.forEach(([key, value]) => plainQueryObject[key] = value);
+
+		const params = {
+			method: httpRequest.method,
+			url: `${httpRequest.protocol}//${httpRequest.hostname}${httpRequest.path}?${new URLSearchParams(plainQueryObject).toString()}`,
+			body: httpRequest.body,
+			headers: httpRequest.headers
+		};
+		console.log('obsidian req params', params);
+
+		const response = await requestUrl(params)
+		console.log('obsidian response', response);
+
+		return {response: {statusCode: response.status, reason: response.text, headers: response.headers}};
+		// return super.handle(request, options);
+	}
 }
 
 export default class PDFPublisherPlugin extends Plugin {
@@ -89,6 +253,9 @@ export default class PDFPublisherPlugin extends Plugin {
 					accessKeyId: this.settings.awsAccessKeyId,
 					secretAccessKey: this.settings.awsSecretAccessKey,
 				},
+				requestHandler: new ObsHttpHandler({
+					requestTimeout: 600000,
+				}),
 				region: this.settings.region,
 			}),
 			params: {Bucket: this.settings.bucketName, Key: key, Body: buffer},
